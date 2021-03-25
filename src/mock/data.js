545e4d627c181bde64ff631b23253753cfa7e839
adferrin <adferrin@icloud.com>
2021-03-25 14:41:52 -0700

    projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'I was a Chef for 13 years starting from as a dishwasher working all the way up to head Chef. Now I am taking on Software development from the ground up.',
+  paragraphTwo: "I graduated from the software immersive program at General Assembly in 2020 for my foundational knowledge. I also have Free Code Camp\'s certifications for JavaScript Algorithms and Data Structures & Front End Libraries. Just I did in the kitchen, I will be solving complex problems with tried and true process coupled with technique.",
+  paragraphThree: 'So far, my skills include JavaScript - Python - MongoDB - PostgreSQL - React - Gatsby - Django - Express - Node.js - RESTFUL Routing - MVC ',
   resume: 'https://docs.google.com/document/d/e/2PACX-1vQO4QwUsAfT3HHwqqBtRxxbKUxfq4d8_sy7YGSuIPCATquvLjFVarfSeScVDJ69dKz1J2xP1sxWv0Af/pub', // if no resume, the button will not show up
 };
 
@@ -30,7 +30,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'NightOwl.jpg',
     title: 'Night Owl',
-    info: 'This is a group project',
+    info: 'This project was a MERN stack and included JSONwebToken, Bcrypt, and Socket.io. This app creates private rooms for groups to search, queue, and dispaly youtube videos in real time using socket.io to chat.',
     info2: '',
     url: 'https://seir629-nightowl.herokuapp.com/',
     repo: 'https://github.com/adferrin/videorange', // if no repo, the button will not show up
@@ -39,19 +39,19 @@ export const projectsData = [
     id: nanoid(),
     img: 'DevCenter.png',
     title: 'Dev Center',
-    info: 'This is a group project',
+    info: 'This appiclicaiton is designed to condense clutter on your computer by authentication with third party API\'s. It allows basic function of those apps and is a full stack app including Python, Django, PostgreSQL, SocialOAuth, PyGitHub, and Tweepy.',
     info2: '',
     url: 'https://devcenter-629.herokuapp.com/',
     repo: 'https://github.com/adferrin/DevCenter', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Racette.png',
+    title: 'Racette',
+    info: 'Racette is a recipe app that creates and stores your favorite foods. This app includes Mongoose, Espress, Materialize, and Google OAuth for sercurity.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://recipe-app-adf.herokuapp.com/',
+    repo: 'https://github.com/adferrin/recipe-app', // if no repo, the button will not show up
   },
 ];
 
